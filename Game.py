51c7c752bafcbe12c0958db41afa5f7ff13b8c3c
David Riddell <davidriddell13@gmail.com>
2020-01-18 13:27:16 +1000

    Gui starting to work on mac and started board

--- Game.py
+++ Game.py
@@ -2,12 +2,15 @@ import pygame
 import time
 
 class Game():
+    # Define some colors
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
+
     def __init__(self):
-        # Define some colors
-        self.black = ( 0, 0, 0)
-        self.white = ( 255, 255, 255)
-        self.green = ( 0, 255, 0)
-        self.red = ( 255, 0, 0)
+        
         self.size = (700, 500)
         self.playing = True
 
@@ -17,20 +20,19 @@ class Game():
         pygame.init()
 
         # Open a new window
-        screen = pygame.display.set_mode(self.size)
-        time.sleep(3)
+        pygame.display.set_mode(self.size)
         pygame.display.set_caption("Sudoku with Auto Solve")
         pygame.display.update()
         
-
+        
     def run_game(self):
         self.init_game()
         
 
         while(self.playing):
-            time.sleep(3)
-            if event.type == pygame.QUIT:
+            event = pygame.event.wait()
+            if event.type == pygame.quit():
                 self.playing = False
-            self.playing = False
+            # self.playing = False
 
         pygame.quit()
--- VideoDriver.py
+++ VideoDriver.py
@@ -6,6 +6,7 @@ def setup_video():
     os.environ['SDL_VIDEO_WINDOW_POS'] = str(position[0]) + "," + str(position[1])
     print(platform.system())
     #picks the platform for the SDL driver
+    #TODO Needs to fix for other platforms that aren't mac
     if platform.system() == 'Windows':
         os.environ['SDL_VIDEODRIVER'] = 'windib'
     if platform.system() == "Linux":
